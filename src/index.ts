8fed9e5566ee7340c381ca8efc7a38c5bdbc48d8
Ramtin Erfani <ramtin.erfani@storykit.io>
2024-04-23 16:17:47 +0200

    update: refactoring

--- src/index.ts
+++ src/index.ts
@@ -10,30 +10,62 @@ const asyncHandler = (fn) =>
     return Promise.resolve(fnReturn).catch(next);
   };
 
-const shadyFunction = () => {
-  throw Error('hello world');
-};
+app.get('/testNext', (req, res, next) => {
+  const throwErr = true;
+  if (throwErr) {
+    // error handler will catch this error, this is because the handler is synchronous
+    throw new Error();
+  }
+  next();
+});
+
+app.get(
+  '/testNextAsync',
+  asyncHandler(async (req, res, next) => {
+    const throwErr = true;
+    if (throwErr) {
+      // error handler will catch this error, this is because the handler is wrapped in a asyncHandler
+      throw new Error();
+    }
+    next();
+  })
+);
 
 app.get('/test', (req, res, next) => {
-  // error handler below will catch this error, this is because the handler is synchronous
-  shadyFunction();
-  next();
+  const throwErr = true;
+  if (throwErr) {
+    // error handler will catch this error, this is because the handler is synchronous
+    throw new Error();
+  }
 });
 
 app.get(
   '/testAsync',
   asyncHandler(async (req, res, next) => {
-    // error handler below will catch this error, this is because the handler is wrapped in a asyncHandler
-    shadyFunction();
-    next();
+    const throwErr = true;
+    if (throwErr) {
+      // error handler will catch this error, this is because the handler is wrapped in a asyncHandler
+      throw new Error();
+    }
   })
 );
 
 app.get('/test2', (req, res, next) => {
-  next(); //throws 404 (not found), because we are using next(), and express is unable to match the next route, so the standard 404 handler would be used
+  res.json({ status: 200 });
+  next();
+});
+
+app.get('/test2', (req, res, next) => {
+  next(); // this will not throw an error, because the response has already been sent
+});
+
+app.get('/test3', (req, res, next) => {
+  next(); //throws 404 (not found), because we are using next(), and express is unable to match the next route, so the standard 404 handler will be used
 });
 
 app.use((err, req, res, next) => {
+  // Error middleware, will catch all errors thrown inside the routes/handlers
+  res.status(500);
   res.json({ status: 500 });
 });
 
